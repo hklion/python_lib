7748db81222c0ef063b5d473c70f21faed865964
Hannah Klion <hklion@berkeley.edu>
2021-04-25 18:55:01 -0700

    update load fonts

--- load_fonts.py
+++ load_fonts.py
@@ -3,5 +3,7 @@ import os
 
 font_dirs = ['{}/fonts/'.format(os.environ['HOME']), ]
 font_files = fm.findSystemFonts(fontpaths=font_dirs)
-font_list = fm.createFontList(font_files)
-fm.fontManager.ttflist.extend(font_list)
+for font_file in font_files:
+  fm.fontManager.addfont(font_file)
+#font_list = fm.createFontList(font_files)
+#fm.fontManager.ttflist.extend(font_list)
