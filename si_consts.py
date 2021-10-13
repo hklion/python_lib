51e75a31ec666c8765b205652b571537f4e57608
Hannah Klion <hannah.klion@gmail.com>
2021-10-13 14:43:32 -0700

    add SI constants

---
+++ si_consts.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+# Physical constants (SI)
+c = 2.99792458e8
+h = 6.6260755e-34
+hbar = h / (2. * np.pi)
+ggrav = 6.67259e-11
+e = 1.6021766e-19
+me = 9.1093897e-31
+mp = 1.6726231e-27
+mn = 1.6749286e-27
+mh = 1.6733e-27
+amu = 1.6605402e-27
+na = 6.0221367e23 # Avogadro's number
+kb = 1.380658e-23
+ev = 1.6021772e-19
+a = 7.5646e-16 # Radiation density constant
+sb = 5.67051e-8 # Stefan-Boltzmann constant
+alpha = 7.29735308e-3 # Fine structure
+ryd = 2.1798741e-18 # Rydberg energy = 13.6 eV
+
+mu0 = 1.25664e-6
+e0 = 8.854187e-12
